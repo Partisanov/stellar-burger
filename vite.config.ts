4cd9005157a6de85d2f2f8530d67e67dd988460c
k.pozharitskiy <k.pozharitskiy@postgrespro.ru>
2024-10-03 00:09:54 +0300

    add deploy to Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: "/ya-practicum-react-burger",
   plugins: [react()],
 });
